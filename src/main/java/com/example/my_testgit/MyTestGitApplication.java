9dc577988c9af2d00581bc7c750277458c8df68e
Oleg Filatov <fillswim@gmail.com>
2022-07-08 08:25:06 +0300

    Initial commit

---
+++ src/main/java/com/example/my_testgit/MyTestGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTestGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_testgit/MyTestGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
